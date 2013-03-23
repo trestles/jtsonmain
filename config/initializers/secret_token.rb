bf63aa1dc4d4d753cb5c9af28fba4a3e59debc89
Jonathan Twaddell <jon@arclocal.com>
2013-03-23 11:33:44 -0700

    ini

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jtsonmain::Application.config.secret_token = 'e209a90d1bd7d69c7cc44380803d2bebdf3d06347d5b058d1d4b4413c6dabdf52edf09e3959ee01133c0b4a69eea56fedeb71b3d7787f0c8fa8b68b880fe8c61'
